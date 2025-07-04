7a5ecf2f9e16d070a5e921c00551a08366e0a74f
Starck Lin <zenyer@gmail.com>
2025-07-04 22:16:09 +0800

    fix(utils): missing file

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,3 +1,7 @@
+// 這個檔案是 @axiom/utils 套件的公開進入點。
+// 它定義了哪些函式可以被其他套件匯入。
+
 export * from './cn';
-export * from './date';
-export * from './format';
+// 如果有其他工具函式，也應在此處匯出，例如：
+// export * from './date';
+// export * from './format';
